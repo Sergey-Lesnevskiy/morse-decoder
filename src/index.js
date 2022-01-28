c316fb9d9eb68aef63f1a2335186d1654fe95c5e
Sergey-Lesnevskiy <sergeybaian@mail.ru>
2022-01-28 16:27:36 +0300

    happened

--- src/index.js
+++ src/index.js
@@ -38,8 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+ 
+
+  let r=expr.match(/.{1,10}/g)
+  let a=10
+  let b=11
+  let morz=''
+  
+  for (let i = 0; i < r.length; i++){
+     
+   var letter=r[i].match(/.{1,2}/g)
+   morz=morz+' '
+   
+  for (let i = 0; i < letter.length; i++){
+    
+  if(letter[i]==a){
+    morz=morz+'.'
+    
+  }else if(letter[i]==b){
+    morz=morz+'-'
+    
+  }else if(letter[i]=='**'){
+    
+    morz=morz+'1'
+  }
+  }
+  }  
+  let res = morz.split(' ')
+    // let test =result
+  
+    var obj = {}
+  var value=''
+       res.splice(0, 1)
+  
+          for (var j = 0; j < res.length; j++) {
+         if(MORSE_TABLE[res[j]]!==undefined) {
+           value=value+MORSE_TABLE[res[j]]
+         }else
+         //   console.log(res[j])
+          value=value+' ' 
+      //console.log(MORSE_TABLE[res[j]])
+          }
+    return  value
+  }
+  
+    
 
 module.exports = {
     decode
